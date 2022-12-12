594fbc5460aaa5b51b0549dedffcf45f0531d7f2
Alexey Volkov <alexey.volkov@ark-kun.com>
2022-12-12 00:16:07 -0800

    refactor(components): Restricted the list of exported members
    *    `create_component_from_func`
    *    `create_graph_component_from_pipeline_func`
    *    `load_component_from_file`
    *    `load_component_from_text`
    *    `load_component_from_url`
    *    `InputPath`
    *    `OutputPath`

--- cloud_pipelines/components/__init__.py
+++ cloud_pipelines/components/__init__.py
@@ -1,1 +1,19 @@
-from .._components.components import *
+from .._components.components import (
+    create_component_from_func,
+    create_graph_component_from_pipeline_func,
+    load_component_from_file,
+    load_component_from_text,
+    load_component_from_url,
+    InputPath,
+    OutputPath,
+)
+
+__all__ = [
+    "create_component_from_func",
+    "create_graph_component_from_pipeline_func",
+    "load_component_from_file",
+    "load_component_from_text",
+    "load_component_from_url",
+    "InputPath",
+    "OutputPath",
+]
