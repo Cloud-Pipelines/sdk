062d1d13519dff1883c1cf0964d5978999096776
Alexey Volkov <alexey.volkov@ark-kun.com>
2022-12-12 01:19:18 -0800

    refactor: Exported the activate_interactive_mode and deactivate_interactive_mode function from the cloud_pipelines module

--- cloud_pipelines/__init__.py
+++ cloud_pipelines/__init__.py
@@ -1,1 +1,8 @@
 from . import components
+from .orchestration import activate_interactive_mode, deactivate_interactive_mode
+
+__all__ = [
+    "activate_interactive_mode",
+    "deactivate_interactive_mode",
+    "components",
+]
